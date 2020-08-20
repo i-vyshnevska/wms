64d725788d8c73b2a957703e7586d0e79e466a8e
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-08-20 10:21:16 +0200

    fixup! Move settings flag from picking type to route

--- delivery_carrier_preference/models/stock_move.py
+++ delivery_carrier_preference/models/stock_move.py
@@ -46,10 +46,8 @@ class StockMove(models.Model):
         res = super().release_available_to_promise()
 
         for picking in self.mapped("picking_id"):
-            pick_type = picking.picking_type_id
-            if (
-                picking.picking_type_code != "outgoing"
-                or not pick_type.force_recompute_preferred_carrier_on_release
+            if picking.picking_type_code != "outgoing" or not picking.mapped(
+                "move_lines.rule_id.route_id.force_recompute_preferred_carrier_on_release"  # noqa
             ):
                 continue
             original_carrier = existing_carriers[picking.id]
